2a634f9163171c785cebef5aa7ee77ca3388833e
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-22 15:48:42 +0530

    code added with the help of sharad kumar

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+// Todo: Make M+ M- and MC functional
+// empty string input
+let string = "";
+
+//accessing every n each button
+let buttons = document.querySelectorAll('.button');
+
+//calls each button element from an array
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    //condition checking if button clicked is '='
+    if(e.target.innerHTML == '='){
+      //eval - executes and evaluates an argument
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    // condition if we clicked on cancel button, input should be empty.
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    // input filled be selected button that is clicked
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
